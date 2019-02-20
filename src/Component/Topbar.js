eb96f359f03fad0f2134de3bfa4740a0cd9447ab
takanorifukuyama <takanori.fukuyama@gmail.com>
2019-02-21 06:28:04 +0900

    add appbar,colortheme

--- src/Component/Topbar.js
+++ src/Component/Topbar.js
@@ -8,6 +8,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+
 const styles = {
   root: {
     flexGrow: 1,
